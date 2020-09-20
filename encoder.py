e7f88cb9ea78aa38d7f08e18c27222f31a173272
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-09-19 18:47:21 -0700

    Fix tabs and spaces

--- encoder.py
+++ encoder.py
@@ -5,7 +5,6 @@ import numpy as np
 import tf2onnx
 from functools import partial
 
-
 LOSS_OUT_FILE = 'Epoch_Loss.txt'
 print("before process_data")
 process_data.process_wav()
@@ -35,18 +34,18 @@ batches = 1
 X = tf.placeholder(tf.float32, shape=[None, inputs])
 l2_regularizer = tf.contrib.layers.l2_regularizer(l2)
 
-autoencoder_dnn = partial(tf.layers.dense, 
-						activation = tf.nn.elu,
-						kernel_initializer = tf.contrib.layers.variance_scaling_initializer(),
-						kernel_regularizer=  tf.contrib.layers.l2_regularizer(l2))
+autoencoder_dnn = partial(tf.layers.dense,
+                          activation=tf.nn.elu,
+                          kernel_initializer=tf.contrib.layers.variance_scaling_initializer(),
+                          kernel_regularizer=tf.contrib.layers.l2_regularizer(l2))
 
 hidden_1 = autoencoder_dnn(X, hidden_1_size)
 hidden_2 = autoencoder_dnn(hidden_1, hidden_2_size)
 hidden_4 = autoencoder_dnn(hidden_2, hidden_3_size)
 hidden_5 = autoencoder_dnn(hidden_4, hidden_2_size)
-outputs =  autoencoder_dnn(hidden_5, inputs, activation=None)
+outputs = autoencoder_dnn(hidden_5, inputs, activation=None)
 
-reconstruction_loss = tf.reduce_mean(tf.square(outputs-X))
+reconstruction_loss = tf.reduce_mean(tf.square(outputs - X))
 reg_loss = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
 loss = tf.add_n([reconstruction_loss] + reg_loss)
 
@@ -54,74 +53,76 @@ optimizer = tf.train.AdamOptimizer(lr)
 training_op = optimizer.minimize(loss)
 init = tf.global_variables_initializer()
 
+
 def next_batch(c_batch, batch_size, sess):
-	ch1_arr = []
-	ch2_arr = []
-	wav_arr_ch1, wav_arr_ch2, sample_rate = process_data.get_next_batch(c_batch, batch_size, sess)
+    ch1_arr = []
+    ch2_arr = []
+    wav_arr_ch1, wav_arr_ch2, sample_rate = process_data.get_next_batch(c_batch, batch_size, sess)
 
-	for sub_arr in wav_arr_ch1:
-		batch_size_ch1 = math.floor(len(sub_arr)/inputs)
-		sub_arr = sub_arr[:(batch_size_ch1*inputs)]
-		ch1_arr.append(np.array(sub_arr).reshape(batch_size_ch1, inputs))
+    for sub_arr in wav_arr_ch1:
+        batch_size_ch1 = math.floor(len(sub_arr) / inputs)
+        sub_arr = sub_arr[:(batch_size_ch1 * inputs)]
+        ch1_arr.append(np.array(sub_arr).reshape(batch_size_ch1, inputs))
 
-	for sub_arr in  wav_arr_ch2:
-		batch_size_ch2 = math.floor(len(sub_arr)/inputs)
-		sub_arr = sub_arr[:(batch_size_ch2*inputs)]
-		ch2_arr.append(np.array(sub_arr).reshape(batch_size_ch2, inputs))
+    for sub_arr in wav_arr_ch2:
+        batch_size_ch2 = math.floor(len(sub_arr) / inputs)
+        sub_arr = sub_arr[:(batch_size_ch2 * inputs)]
+        ch2_arr.append(np.array(sub_arr).reshape(batch_size_ch2, inputs))
 
-	return np.array(ch1_arr), np.array(ch2_arr), sample_rate
+    return np.array(ch1_arr), np.array(ch2_arr), sample_rate
 
 
-##### Run training
+# Run training
 with tf.Session() as sess:
-	init.run()
-	
-	for epoch in range(epochs):
-		epoch_loss = []
-		print("Epoch: " + str(epoch))
-		for i in range(batches):
-			ch1_song, ch2_song, sample_rate = next_batch(i, batch_size, sess)
-			total_songs = np.hstack([ch1_song, ch2_song])
-			batch_loss = []
+    init.run()
 
-			for j in range(len(total_songs)):
-				x_batch = total_songs[j]
-				_, l = sess.run([training_op, loss], feed_dict={X:x_batch})
-				batch_loss.append(l)
-				print("Song loss: " + str(l))
+    for epoch in range(epochs):
+        epoch_loss = []
+        print("Epoch: " + str(epoch))
+        for i in range(batches):
+            ch1_song, ch2_song, sample_rate = next_batch(i, batch_size, sess)
+            total_songs = np.hstack([ch1_song, ch2_song])
+            batch_loss = []
 
-			print("Curr Epoch: " + str(epoch) + " Curr Batch: " + str(i) + "/"+ str(batches))
-			print("Batch Loss: " + str(np.mean(batch_loss)))
-			epoch_loss.append(np.mean(batch_loss))
+            for j in range(len(total_songs)):
+                x_batch = total_songs[j]
+                _, l = sess.run([training_op, loss], feed_dict={X: x_batch})
+                batch_loss.append(l)
+                print("Song loss: " + str(l))
 
-		print("Epoch Avg Loss: " + str(np.mean(epoch_loss)))
+            print("Curr Epoch: " + str(epoch) + " Curr Batch: " + str(i) + "/" + str(batches))
+            print("Batch Loss: " + str(np.mean(batch_loss)))
+            epoch_loss.append(np.mean(batch_loss))
 
-		if epoch % 1000 == 0:
-			ch1_song_new, ch2_song_new, sample_rate_new = next_batch(2, 1, sess)
-			x_batch = np.hstack([ch1_song_new, ch2_song_new])[0]
-			print("Sample rate: " + str(sample_rate_new))
+        print("Epoch Avg Loss: " + str(np.mean(epoch_loss)))
 
-			orig_song = []
-			full_song = []
-			evaluation = outputs.eval(feed_dict={X: x_batch})
-			print("Output: " + str(evaluation))
-			full_song.append(evaluation)
-			orig_song.append(x_batch)
+        if epoch % 1000 == 0:
+            ch1_song_new, ch2_song_new, sample_rate_new = next_batch(2, 1, sess)
+            x_batch = np.hstack([ch1_song_new, ch2_song_new])[0]
+            print("Sample rate: " + str(sample_rate_new))
 
-			# Merge the nested arrays
-			orig_song = np.hstack(orig_song)
-			full_song = np.hstack(full_song)
+            orig_song = []
+            full_song = []
+            evaluation = outputs.eval(feed_dict={X: x_batch})
+            print("Output: " + str(evaluation))
+            full_song.append(evaluation)
+            orig_song.append(x_batch)
 
-			# Compute and split the channels
-			orig_song_ch1 = orig_song[:math.floor(len(orig_song)/2)]
-			orig_song_ch2 = orig_song[math.floor(len(orig_song)/2):]
-			full_song_ch1 = full_song[:math.floor(len(full_song)/2)]
-			full_song_ch2 = full_song[math.floor(len(full_song)/2):]
+            # Merge the nested arrays
+            orig_song = np.hstack(orig_song)
+            full_song = np.hstack(full_song)
 
-			# Save both the untouched song and reconstructed song to the 'output' folder
-			process_data.save_to_wav(full_song_ch1, full_song_ch2, sample_rate, orig_song_ch1, orig_song_ch2, epoch, 'output', sess)
+            # Compute and split the channels
+            orig_song_ch1 = orig_song[:math.floor(len(orig_song) / 2)]
+            orig_song_ch2 = orig_song[math.floor(len(orig_song) / 2):]
+            full_song_ch1 = full_song[:math.floor(len(full_song) / 2)]
+            full_song_ch2 = full_song[math.floor(len(full_song) / 2):]
 
-	onnx_graph = tf2onnx.tfonnx.process_tf_graph(sess.graph, input_names=["input:0"], output_names=["output:0"])
-	model_proto = onnx_graph.make_model("test")
-	with open("output/model.onnx", "wb") as f:
-		f.write(model_proto.SerializeToString())
+            # Save both the untouched song and reconstructed song to the 'output' folder
+            process_data.save_to_wav(full_song_ch1, full_song_ch2, sample_rate, orig_song_ch1, orig_song_ch2, epoch,
+                                     'output', sess)
+
+    onnx_graph = tf2onnx.tfonnx.process_tf_graph(sess.graph, input_names=["input:0"], output_names=["output:0"])
+    model_proto = onnx_graph.make_model("test")
+    with open("output/model.onnx", "wb") as f:
+        f.write(model_proto.SerializeToString())
--- process_data.py
+++ process_data.py
@@ -13,89 +13,89 @@ DATA_FILES_WAV = 'audio_wav'
 file_arr = []
 curr_batch = 0
 
+
 def convert_mp3_to_wav():
-	index = 0
-	for file in iglob(DATA_FILES_MP3 + '/*.mp3'):
-                print("begin convert " + file)
-                mp3_to_wav = AudioSegment.from_mp3(file)
-                print("finished convert " + file+ ". Now exporting file to audio_wav")
-                mp3_to_wav.export(DATA_FILES_WAV+'/'+str(index)+'.wav', format='wav')
-                print("export to audio_wav for " + file+ " success")
-                index += 1
+    index = 0
+    for file in iglob(DATA_FILES_MP3 + '/*.mp3'):
+        print("begin convert " + file)
+        mp3_to_wav = AudioSegment.from_mp3(file)
+        print("finished convert " + file + ". Now exporting file to audio_wav")
+        mp3_to_wav.export(DATA_FILES_WAV + '/' + str(index) + '.wav', format='wav')
+        print("export to audio_wav for " + file + " success")
+        index += 1
+
 
 def process_wav():
-	convert_mp3_to_wav()
-	file_range = 0
-	for file in iglob(DATA_FILES_WAV +'/*.wav'):
-		file_arr.append(file)
+    convert_mp3_to_wav()
+    file_range = 0
+    for file in iglob(DATA_FILES_WAV + '/*.wav'):
+        file_arr.append(file)
+
 
 def get_next_batch(curr_batch, songs_per_batch, sess):
-	wav_arr_ch1 = []
-	wav_arr_ch2 = []
-	if (curr_batch) >= (len(file_arr)):
-		curr_batch = 0
+    wav_arr_ch1 = []
+    wav_arr_ch2 = []
+    if (curr_batch) >= (len(file_arr)):
+        curr_batch = 0
 
-	start_position = curr_batch * songs_per_batch
-	end_position = start_position + songs_per_batch
-	for idx in range(start_position, end_position):
-		try:
-			audio_binary = tf.read_file(file_arr[idx])
-		except:
-			print(file_arr)
-			exit(0)
-		wav_decoder = audio_ops.decode_wav(
-			audio_binary,
-			desired_channels=2)
-		sample_rate, audio = sess.run([wav_decoder.sample_rate, wav_decoder.audio])
-		audio = np.array(audio)
+    start_position = curr_batch * songs_per_batch
+    end_position = start_position + songs_per_batch
+    for idx in range(start_position, end_position):
+        try:
+            audio_binary = tf.read_file(file_arr[idx])
+        except:
+            print(file_arr)
+            exit(0)
+        wav_decoder = audio_ops.decode_wav(
+            audio_binary,
+            desired_channels=2)
+        sample_rate, audio = sess.run([wav_decoder.sample_rate, wav_decoder.audio])
+        audio = np.array(audio)
 
-		if len(audio[:, 0]) != 5292000: 
-			continue
+        if len(audio[:, 0]) != 5292000:
+            continue
 
-		wav_arr_ch1.append(rfft(audio[:,0]))
-		wav_arr_ch2.append(rfft(audio[:,1]))
-		print("Returning File: " + file_arr[idx])
-		
+        wav_arr_ch1.append(rfft(audio[:, 0]))
+        wav_arr_ch2.append(rfft(audio[:, 1]))
+        print("Returning File: " + file_arr[idx])
 
-	return wav_arr_ch1, wav_arr_ch2, sample_rate
+    return wav_arr_ch1, wav_arr_ch2, sample_rate
+
 
 def save_to_wav(audio_arr_ch1, audio_arr_ch2, sample_rate, original_song_ch1, original_song_ch2, idty, folder, sess):
-	audio_arr_ch1 = irfft(np.hstack(np.hstack(audio_arr_ch1)))
-	audio_arr_ch2 = irfft(np.hstack(np.hstack(audio_arr_ch2)))
+    audio_arr_ch1 = irfft(np.hstack(np.hstack(audio_arr_ch1)))
+    audio_arr_ch2 = irfft(np.hstack(np.hstack(audio_arr_ch2)))
 
-	original_song_ch1 = irfft(np.hstack(np.hstack(original_song_ch1)))
-	original_song_ch2 = irfft(np.hstack(np.hstack(original_song_ch2)))
-	
-	original_song = np.hstack(np.array((original_song_ch1, original_song_ch2)).T)
-	audio_arr = np.hstack(np.array((audio_arr_ch1, audio_arr_ch2)).T)
+    original_song_ch1 = irfft(np.hstack(np.hstack(original_song_ch1)))
+    original_song_ch2 = irfft(np.hstack(np.hstack(original_song_ch2)))
 
-	print(original_song)
-	w = np.linspace(0, sample_rate, len(audio_arr))
-	w = w[0:len(audio_arr)]
-	plt.figure(1)
+    original_song = np.hstack(np.array((original_song_ch1, original_song_ch2)).T)
+    audio_arr = np.hstack(np.array((audio_arr_ch1, audio_arr_ch2)).T)
 
-	plt.plot(w, original_song)
-	plt.savefig(str(folder) + '/original.png')
-	plt.plot(w, audio_arr)
-	plt.xlabel('sample')
-	plt.ylabel('amplitude')
-	plt.savefig(str(folder) + '/compressed' + str(idty) + '.png')
-	plt.clf()	
+    print(original_song)
+    w = np.linspace(0, sample_rate, len(audio_arr))
+    w = w[0:len(audio_arr)]
+    plt.figure(1)
 
-	cols = 2
-	rows = math.floor(len(audio_arr)/2)
-	audio_arr = audio_arr.reshape(rows, cols)
-	original_song = original_song.reshape(rows, cols)
+    plt.plot(w, original_song)
+    plt.savefig(str(folder) + '/original.png')
+    plt.plot(w, audio_arr)
+    plt.xlabel('sample')
+    plt.ylabel('amplitude')
+    plt.savefig(str(folder) + '/compressed' + str(idty) + '.png')
+    plt.clf()
 
-	wav_encoder = ffmpeg.encode_audio(
-		audio_arr, file_format='wav', samples_per_second=sample_rate)
-	wav_encoder_orig = ffmpeg.encode_audio(
-		original_song, file_format='wav', samples_per_second=sample_rate)
+    cols = 2
+    rows = math.floor(len(audio_arr) / 2)
+    audio_arr = audio_arr.reshape(rows, cols)
+    original_song = original_song.reshape(rows, cols)
 
-	wav_file = sess.run(wav_encoder)
-	wav_orig = sess.run(wav_encoder_orig)
-	open(str(folder)+'/out.wav', 'wb').write(wav_file)
-	open(str(folder)+'/wav_orig.wav', 'wb').write(wav_orig)
+    wav_encoder = ffmpeg.encode_audio(
+        audio_arr, file_format='wav', samples_per_second=sample_rate)
+    wav_encoder_orig = ffmpeg.encode_audio(
+        original_song, file_format='wav', samples_per_second=sample_rate)
 
-
-
+    wav_file = sess.run(wav_encoder)
+    wav_orig = sess.run(wav_encoder_orig)
+    open(str(folder) + '/out.wav', 'wb').write(wav_file)
+    open(str(folder) + '/wav_orig.wav', 'wb').write(wav_orig)
